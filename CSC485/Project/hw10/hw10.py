67893c0e3b6309a9b4cb02cc41c7d73ab1eabf8e
Sahil Idnani <idnanisahu@gmail.com>
2023-10-20 13:58:30 -0400

    hw 10: Add the application code
    projects/hw10/hw10.py
    + Added the homework assignment application code

---
+++ CSC485/Project/hw10/hw10.py
@@ -0,0 +1,11 @@
+def is_it_a_fruit(fruit_candidate):
+    fruits = ['apple', 'pear', 'bananna', 'grape']
+
+    if fruit_candidate in fruits:
+        return True
+    else:
+        return False
+
+
+
+
