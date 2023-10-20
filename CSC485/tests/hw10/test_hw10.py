987f455d61167d9668b28060e35eda86173b53da
Sahil Idnani <idnanisahu@gmail.com>
2023-10-20 14:35:09 -0400

    hw 10: Begin tests
    tests/hw10/test_hw10.py
    + Added two test classes and two methods.
    1: Class- TestShouldReturnTrue,
      method- def test_extact_match_apple(self):
    2: class TestShouldReturnFalse(object):
      method- def test_case_difference(self):

---
+++ CSC485/tests/hw10/test_hw10.py
@@ -0,0 +1,37 @@
+import pytest
+from CSC485.Project.hw10.hw10 import is_it_a_fruit
+
+
+"""Testable things
+Input-
+    String
+          Good
+              Perfect match with a string
+          Bad
+              Change in Cases
+              Blank space between characters
+              Empty strings
+Output-
+    True
+        Exact Match
+    False
+        Everything else    
+
+
+"""
+
+
+class TestShouldReturnTrue(object):
+
+    def test_extact_match_apple(self):
+        result = is_it_a_fruit('apple')
+        assert result == True
+
+
+class TestShouldReturnFalse(object):
+
+    def test_case_difference(self):
+        result = is_it_a_fruit('ApplE')
+        assert result == False
+
+
