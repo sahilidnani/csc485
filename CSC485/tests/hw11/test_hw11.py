879e5fbcb5bc96882caefcdfdd5824cfc6d8b1ad
Sahil Idnani <idnanisahu@gmail.com>
2023-11-05 17:05:00 -0500

    hw11: Use pytest.raises hw11: Modify test cases with pytest.raises format
    tests/hw11/hw11.py
    + Modified the following methods:
    def test_invalid_input
    test_missing_input
    test_valid_input_with_whitespace
    test_float_input
    test_multiple_input
    test_nonalphanumeric_char
    test_1valid1invalidinput

--- CSC485/tests/hw11/test_hw11.py
+++ CSC485/tests/hw11/test_hw11.py
@@ -6,46 +6,39 @@ def test_valid_input():
     assert get_formal_name('apple') == 'Malus domestica'
     assert get_formal_name('banana') == 'Musa acuminata'
 
+
 def test_invalid_input():
     with pytest.raises(KeyError):
         get_formal_name('tomato')
 
+
 def test_missing_input():
-        with pytest.raises(KeyError):
-             get_formal_name('')
+    with pytest.raises(KeyError):
+        get_formal_name('')
+
 
 def test_valid_input_with_whitespace():
-    assert get_formal_name('  banana  ') == 'Musa acuminata'
-    assert get_formal_name('   lemon') == 'Citrus limon'
+    with pytest.raises(KeyError):
+        get_formal_name('ban an a')
+
+
 
 def test_float_input():
-    assert get_formal_name('4.64')
-    if TypeError:
-        assert True
-    else:
-        assert False
+    with pytest.raises(KeyError):
+        get_formal_name('4.64')
+
+
 
 def test_multiple_input():
-    assert get_formal_name('apple', 'banana')
-    assert result
+    with pytest.raises(KeyError):
+        get_formal_name('apple', 'banana')
+
 
 def test_nonalphanumeric_char():
-    assert get_formal_name('$@')
-    assert result
+    with pytest.raises(KeyError):
+            get_formal_name('$@')
+
 
 def test_1valid1invalidinput():
-    assert get_formal_name('apple, tomato')
-    assert result
-
-
-
-
-
-
-
-
-
-
-
-
-
+    with pytest.raises(TypeError):
+        get_formal_name('apple', 'tomato')
